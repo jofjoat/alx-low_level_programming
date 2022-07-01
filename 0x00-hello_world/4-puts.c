bebd4680eb4bc083146c5ddcb570e95d22e47d32
JOF <jay4jerry@icloud.com>
2022-06-30 20:10:28 -0700

    update scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* A small program that prints with the puts function */
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise 
+ */
 
 int main(void)
 {
