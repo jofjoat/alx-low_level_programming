8ac94a2b2a74e2e4a2b699ed27b086680976e072
JOF <jay4jerry@icloud.com>
2022-07-01 01:42:11 -0700

    update on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-
-
 /**
- * main - prints a phrase 
+ * main - prints a phrase
  * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
