4699b7ff434c20bda976dd075cb3bc9bac7d0d62
JOF <jay4jerry@icloud.com>
2022-07-01 05:27:50 -0700

    new scripts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+ * main - a program that prints a phrase
+ * Return: 1 as output
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}	
