6999e2dd67b0664f8e5b3c93a94541cdd4288ece
JOF <jay4jerry@icloud.com>
2022-07-01 01:48:55 -0700

    new scripts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/* A program to print with the printf function */
+/**
+ * main - program to print with the printf function
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
