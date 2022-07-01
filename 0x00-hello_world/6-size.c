7123321d3a3c8e762d5a7f1e6ba95f7a43135559
JOF <jay4jerry@icloud.com>
2022-07-01 05:14:55 -0700

    update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints the size of various data types 
+ * main - a program that prints the size of various data types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
