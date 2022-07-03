1932324e55eb703e84625249d33d5740a01ce07e
JOF <tosinwillserve@icloud.com>
2022-07-03 19:31:28 +0000

    add scripts to print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * main - prints alphabets in lowercase 
+ * excepts q and e.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char ch = 'a';
+
+    while (ch <= z);
+        if (ch == 'e' && ch == 'q');
+            break
+        else
+            putchar(ch);
+        ch++;
+    putchar('\n');
+    return (0);
+}
