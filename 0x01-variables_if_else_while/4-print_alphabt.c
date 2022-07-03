e61efd26c4e5ea6005e3a94c27a09297763765be
JOF <tosinwillserve@icloud.com>
2022-07-03 19:47:22 +0000

    new scripts

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,14 @@ int main(void)
 {
     char ch = 'a';
 
-    while (ch <= 'z'; ch++)
-    {
-        if (ch == 'e' && ch == 'q')
-        {
-            break
+    while (ch <= 'z') {
+        if (ch == 'e' && ch == 'q') {
+            break;
         }
         else {
             putchar(ch);
         }
+        ch++;
     }
     putchar('\n');
     return (0);
