292dcff8246399513ee8efd1d78282191ac1e626
JOF <tosinwillserve@icloud.com>
2022-07-03 19:56:01 +0000

    new scripts

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-/*
- * main - prints alphabets in lowercase 
- * excepts q and e.
- * Return: Always 0 (Success)
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
-
 int main(void)
 {
-    char ch = 'a';
+	char ch = 'a';
 
-    while (ch <= 'z')
-    {
-        if (ch != 'e' && ch != 'q')
-        {
-            putchar(ch);
-        }
-        ch++;
-    }
-    putchar('\n');
-    return (0);
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
