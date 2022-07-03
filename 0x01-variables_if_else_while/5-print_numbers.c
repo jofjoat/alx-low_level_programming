fb3af2aa0f638e9745d42cb7d833eb2ba8acb7bf
JOF <tosinwillserve@icloud.com>
2022-07-03 21:00:18 +0000

    script that prints all single base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the base 10 single digits,
+ * below the number 10
+ * Return: Aways 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+        n++
+	}
+	printf('\n');
+	return (0);
+}
