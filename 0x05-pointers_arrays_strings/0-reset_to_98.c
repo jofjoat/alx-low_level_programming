036e2ccbba16b6fb18dc0a147ed90cf2d3a416c1
jof <jay4jerry@icloud.com>
2022-07-12 08:00:51 +0000

    new scripts

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function that prints a pointer
- * as a parameter and updates it's value
+ * reset_to_98 - takes a pointer and updates the value it point to
  * @n: pointer
- * Return: Always 0.
+ * Return: void
  */
+
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
