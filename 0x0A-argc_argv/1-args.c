62779b8353d0ea43493e54bf095954ef54fdfb02
jof <jay4jerry@icloud.com>
2022-07-26 17:56:41 +0000

    new scripts

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
  * @argv: array that points to the arguments
  * Return: Always 0
  */
-int main(int argc, char *argv[])
-{    
-    printf("%i\n", argv - 1);
+int main(int argc, char const *argv[])
+{
+    (void)argv;
+    printf("%i\n", argc - 1);
     return (0);
 }
