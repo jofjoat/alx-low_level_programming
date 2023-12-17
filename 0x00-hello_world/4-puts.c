1a875e95532d5bbc5629aa742658b1a7429dd9aa
jay <jay4jerry@icloud.com>
2023-12-17 13:11:28 -0800

    Script that displays a phrase

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a simple text.
+ *
+ * Return: 0 ends the program.
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
