6226e6d53045d6093db2506d27d9c61e743f23da
jay <jay4jerry@icloud.com>
2023-12-17 15:09:17 -0800

    C Program that display a phrase without using printf or puts func.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - a program that prints a phrase.
+ * Return: 1 end program indicating errors.
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
