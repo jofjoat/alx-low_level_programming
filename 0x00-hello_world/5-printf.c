633618954d68dd718e3031d5be8c85806f571eac
jay <jay4jerry@icloud.com>
2023-12-17 13:26:59 -0800

    Script that displays a phrase using the printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints a phrase.
+ * Return: 0 ends the program.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
