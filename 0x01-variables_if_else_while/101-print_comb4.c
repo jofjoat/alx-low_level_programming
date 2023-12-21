0f0c22bbc6f02a7f4895460f430f20a58838e728
jay <jay4jerry@icloud.com>
2023-12-21 19:52:49 +0000

    A program that prints all possible comb of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,17 +13,21 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; k++)
+		for (j = 0; j < 10; j++)
 		{
-			if (i != j && i != k && j != k)
+			for (k = 0; k < 10; k++)
 			{
-				if (i < j && j < k)
+				if (i != j && i != k && j != k)
 				{
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(k + '0');
-					putchar(',');
-					putchar(' ');
+					if (i < j && j < k)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(k + '0');
+						putchar(',');
+						putchar(' ');
+				
+					}	
 				}
 			}
 		}
