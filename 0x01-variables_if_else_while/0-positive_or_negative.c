0d681c387f4bc0823d6d03832a374fdf7aea8200
jay <jay4jerry@icloud.com>
2023-12-21 08:18:04 -0800

    A program that returns a result if a condition is met

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - a program that returns a result if
+ * a condition is met.
+ * Return: 0 ends the program successfully.
+*/
 int main(void)
 {
-	
 	int n;
 
-    	srand(time(0));
-    	n = rand() - RAND_MAX / 2;
-    	/* your code goes there */
-   	if (n > 0)
-    	{
-        	printf("%d is positive\n", n);
-    	}
-    	if (n == 0)
-    	{
-        	printf("%d is zero\n", n);
-    	}
-    	else
-    	{
-        	printf("%d is negative\n", n);
-    	}
-    	return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n");
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n");
+	}
 }
