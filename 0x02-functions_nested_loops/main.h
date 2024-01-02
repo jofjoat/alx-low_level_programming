fe2281055d5f5d56f0a10bdb41ac0f82657c86fe
jay <jay4jerry@icloud.com>
2024-01-02 13:10:08 -0800

    C Header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,7 @@
-//
-// Created by JOF on 1/2/2024.
-//
-
 #ifndef UNTITLED_MAIN_H
 #define UNTITLED_MAIN_H
 
-// functions declaration
+// function declarations
 int _putchar(char c);
 
 #endif //UNTITLED_MAIN_H
