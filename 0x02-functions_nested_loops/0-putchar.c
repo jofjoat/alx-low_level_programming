0266291a46a2c4da8ff8c9d4fa04eb3610268ae6
jay <jay4jerry@icloud.com>
2024-01-02 13:02:32 -0800

    A script that prints a word

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,15 @@
  * main - a program that prints "_putchar"
  * Return: 0 ends a program
 */
-int main()
+int main(void)
 {
 	int i = 0;
 	char text[9] = "_putchar";
 
-	for(i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(text[i]);
 	}
 	_putchar('\n');
-	return(0);
+	return (0);
 }
